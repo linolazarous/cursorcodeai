a21edeb39ae8422c777a232fdfcc0b45bf442059
linolazarous <linolaku560@gmail.com>
2026-02-24 08:04:35 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,9 +2,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-/**
- * Fix __dirname in ES module scope
- */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
